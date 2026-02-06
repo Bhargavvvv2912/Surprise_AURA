13d116d4d786ca9d4b77213c3e9028705cc50b7c
Bhargav <bhargavksrinidhi@gmail.com>
2026-02-06 15:06:19 -0500

    Files added

--- dependency_agent.py
+++ dependency_agent.py
@@ -31,7 +31,7 @@ AGENT_CONFIG = {
     "PRIMARY_REQUIREMENTS_FILE": "primary_requirements.txt",
     "VALIDATION_CONFIG": {
         "type": "script",
-        "smoke_test_script": "validation_suprise.py",
+        "smoke_test_script": "validation_surprise.py",
         "project_dir": "." 
     },
     "MAX_RUN_PASSES": 3,
